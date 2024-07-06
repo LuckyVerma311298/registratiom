73243a761fdfe48a18d70eb0b2d8f843a236eefe
LuckyVerma311298 <l.v814651@gmail.com>
2024-07-06 13:45:03 +0530

    javascript.js

---
+++ javascript.js
@@ -0,0 +1,4 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thanks for acadmic registration year 2024-2025. Your cuims we start after 10 min .You can login your cuims after 10 min . ');
+});
